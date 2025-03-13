8c079311265fafaf78151942330dceb2e171f335
nasc1mento <anr@discente.ifpe.edu.br>
2025-03-13 01:11:19 -0300

    fix: send pages as string fixed

--- src/api/livros.js
+++ src/api/livros.js
@@ -1,12 +1,14 @@
 export const salvarLivro = async (title, author, genre, pages) => {
     const token = localStorage.getItem('token'); 
+    pages = parseInt(pages, 10)
+    const body = { title, author, genre, pages }
     const response = await fetch(`${process.env.REACT_APP_URL_API}/books`, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
-             'Authorization': `Bearer ${token}`
+            'Authorization': `Bearer ${token}`
         },
-        body: JSON.stringify({ title, author, genre, pages })
+        body: JSON.stringify(body)
     });
 
     return response;
